cd24cd77a587ee06119bb96558d7a3e5193527fa
Cedric Lechevrel <cedrelek@gmail.com>
2015-11-09 23:34:39 +0100

    Add the wall definition

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-angular.module("ZomeBuilder", ["ViewModule",
-                               "ZomeDefinitionModule",
-                               "ZomeViewModule",
-                               "3DViewModule"])
-  .directive("zomeViewComp", function() {
+angular.module("ZomeBuilder", ['ViewModule',
+                               'ZomeDefinitionModule',
+                               'ZomeViewModule',
+                               '3DViewModule',
+                               'FloorDefinitionModule',
+                             'WallDefinitionModule'])
+  .directive('zomeViewComp', function() {
     return {
       templateUrl: 'components/zomeView.html',
       restrict: 'E',
       replace: true
+    }
+  })
+  .directive('floorDefinitionComp', function() {
+    return {
+      templateUrl : 'components/floorDefinition.html',
+      restrict : 'E',
+      replace : true
+    }
+  })
+  .directive('wallDefinitionComp', function() {
+    return {
+      templateUrl : 'components/wallDefinition.html',
+      restrict : 'E',
+      replace : true
     }
   });
